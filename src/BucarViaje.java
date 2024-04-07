92e24d719e9bf274b731de84e54f34d7a67c66cc
SamuelAlonso05 <166213301+SamuelAlonso05@users.noreply.github.com>
2024-04-07 16:50:10 +0200

    Add files via upload

---
+++ src/BucarViaje.java
@@ -0,0 +1,7 @@
+public class BucarViaje {
+    public void buscarViajesDisponibles(String puntoOrigen, String puntoDestino) {
+
+        System.out.println("Buscando viajes disponibles desde " + puntoOrigen + " hasta " + puntoDestino);
+        System.out.println("No se encontraron viajes disponibles para el viaje desde " + puntoOrigen + " hasta " + puntoDestino);
+    }
+}
---
+++ src/PublicarViaje.java
@@ -0,0 +1,11 @@
+public class PublicarViaje {
+    public void publicarViaje(String puntoOrigen, String puntoDestino, String fecha, int plazasDisponibles) {
+
+        System.out.println("Viaje publicado con éxito:");
+        System.out.println("Origen: " + puntoOrigen);
+        System.out.println("Destino: " + puntoDestino);
+        System.out.println("Fecha: " + fecha);
+        System.out.println("Plazas disponibles: " + plazasDisponibles);
+
+    }
+}
